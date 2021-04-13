7459c4e7b6f5dcc591ad9d2b6b2c22e347de4cf7
sharifulislamshaugh <sisbd24@gmail.com>
2021-04-13 13:17:52 +0600

    with splash screen

--- app/src/main/java/com/example/bunonbasket/ActivityHome.kt
+++ app/src/main/java/com/example/bunonbasket/ActivityHome.kt
@@ -2,13 +2,10 @@ package com.example.bunonbasket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
 
-class MainActivity : AppCompatActivity() {
+class ActivityHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Remove title bar
-        getSupportActionBar()!!.hide();
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
