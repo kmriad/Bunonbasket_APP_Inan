c8b87268202a08f915ea45448f99460ca8b859f7
inanmahmud92 <inan.mahmud1992@gmail.com>
2021-04-13 14:22:28 +0600

    status bar hidden

--- app/src/main/java/com/example/bunonbasket/MainActivity.kt
+++ app/src/main/java/com/example/bunonbasket/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.bunonbasket
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Remove title bar
-        getSupportActionBar()!!.hide();
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        supportActionBar?.hide();
         setContentView(R.layout.activity_main)
     }
 }
