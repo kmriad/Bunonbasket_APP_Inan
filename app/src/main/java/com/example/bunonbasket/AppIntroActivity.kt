586ec47e63ff0ad06093d15a864f53565cfb23b2
sharifulislamshaugh <sisbd24@gmail.com>
2021-04-13 17:06:04 +0600

    with splash screen and app intro

--- app/src/main/java/com/example/bunonbasket/AppIntroActivity.kt
+++ app/src/main/java/com/example/bunonbasket/AppIntroActivity.kt
@@ -1,53 +1,78 @@
 package com.example.bunonbasket
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.github.appintro.AppIntro
 import com.github.appintro.AppIntroFragment
 
+
 class AppIntroActivity : AppIntro() {
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        supportActionBar?.hide();
         // Make sure you don't call setContentView!
 
         // Call addSlide passing your Fragments.
         // You can use AppIntroFragment to use a pre-built fragment
         addSlide(
             AppIntroFragment.newInstance(
-                title = "Welcome...",
-                description = "This is the first slide of the example",
-                imageDrawable = R.drawable.bb_logo
+                backgroundDrawable = R.drawable.ic_on_boarding_1
             )
         )
         addSlide(
             AppIntroFragment.newInstance(
-                title = "...Let's get started!",
-                description = "This is the last slide, I won't annoy you more :)",
-                imageDrawable = R.drawable.bb_logo
+                backgroundDrawable = R.drawable.ic_on_boarding_2
+            )
+        )
+        addSlide(
+            AppIntroFragment.newInstance(
+                backgroundDrawable = R.drawable.ic_on_boarding_3
+            )
+        )
+        addSlide(
+            AppIntroFragment.newInstance(
+                backgroundDrawable = R.drawable.ic_on_boarding_4
             )
         )
         // Toggle Indicator Visibility
         isIndicatorEnabled = true
 
-// Change Indicator Color
+        // Change Indicator Color
         setIndicatorColor(
             selectedIndicatorColor = getColor(R.color.orange),
             unselectedIndicatorColor = getColor(R.color.gray)
         )
+        // Control the status bar color
+        setColorDoneText(R.color.black)
+        setDoneText("SHOP NOW")
+        setColorSkipButton(R.color.black)
+        showSeparator(false)
+        getDrawable(R.drawable.ic_next)?.let { setImageNextButton(it) }
     }
 
     override fun onSkipPressed(currentFragment: Fragment?) {
         super.onSkipPressed(currentFragment)
         // Decide what to do when the user clicks on "Skip"
+        val intent = Intent(this,ActivityHome::class.java)
+        startActivity(intent)
         finish()
     }
 
     override fun onDonePressed(currentFragment: Fragment?) {
         super.onDonePressed(currentFragment)
         // Decide what to do when the user clicks on "Done"
+        val intent = Intent(this,ActivityHome::class.java)
+        startActivity(intent)
         finish()
     }
 }
--- app/src/main/java/com/example/bunonbasket/MainActivity.kt
+++ app/src/main/java/com/example/bunonbasket/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.bunonbasket
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var TIME_OUT:Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         supportActionBar?.hide();
         setContentView(R.layout.activity_main)
+        loadSplashScreen()
+    }
+
+    private fun loadSplashScreen(){
+        Handler().postDelayed({
+            // You can declare your desire activity here to open after finishing splash screen. Like MainActivity
+            val intent = Intent(this,AppIntroActivity::class.java)
+            startActivity(intent)
+            finish()
+        },TIME_OUT)
     }
 }
