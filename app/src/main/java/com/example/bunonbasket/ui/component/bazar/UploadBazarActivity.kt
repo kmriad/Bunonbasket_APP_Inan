e757f8d1d6e58204d759865186533914b223ff47
inanmahmud92 <inan.mahmud1992@gmail.com>
2021-05-20 16:31:53 +0600

    updated

--- app/src/main/java/com/example/bunonbasket/ui/component/bazar/UploadBazarActivity.kt
+++ app/src/main/java/com/example/bunonbasket/ui/component/bazar/UploadBazarActivity.kt
@@ -1,17 +1,29 @@
 package com.example.bunonbasket.ui.component.bazar
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.core.app.NavUtils
+import android.view.SurfaceView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.bunonbasket.R
 import com.example.bunonbasket.databinding.ActivityUploadBazarBinding
+import com.tbruyelle.rxpermissions3.RxPermissions
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.Disposable
+
 
 class UploadBazarActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityUploadBazarBinding
+    private val camera: CameraManager? = null
+
+    val rxPermissions = RxPermissions(this)
+    private val surfaceView: SurfaceView? = null
+    private val disposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +32,21 @@ class UploadBazarActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+
+        binding.btnCamera.setOnClickListener {
+            Observable.just(checkIsGranted(Manifest.permission.CAMERA))
+                .flatMap { granted ->
+                    if (granted)
+                        Observable.just(true)
+                    else rxPermissions.request(Manifest.permission.CAMERA)
+                }
+                .subscribe {
+
+                }
+        }
+    }
+
+    fun checkIsGranted(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
     }
 }
